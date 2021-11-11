dbe81f4639110ab20f338b7940ffb88fa2608d5d
Gota7 <gotaseven@gmail.com>
2021-11-11 12:40:03 -0500

    Make Program Command Line
    It's alive.

--- Program.cs
+++ Program.cs
@@ -7,10 +7,29 @@ namespace Ndst {
     class Program {
 
         static void Main(string[] args) {
-            // ROM n = new ROM("NSMB.nds");
-            // n.Extract("ROM");
-            ROM r = new ROM("ROM", "Patch");
-            r.Save("Test.nds");
+            if (args.Length < 3) {
+                PrintUsage();
+            } else {
+                if (args[0].Equals("-e")) {
+                    ROM r = new ROM(args[1]);
+                    r.Extract(args[2]);
+                } else if (args[0].Equals("-p")) {
+                    ROM r = new ROM(args[1], args[2]);
+                    r.Save(args[3]);
+                } else {
+                    PrintUsage();
+                }
+            }
+        }
+
+        static void PrintUsage() {
+            Console.WriteLine("NDS Toolkit");
+            Console.WriteLine("  c2021 Gota7\n");
+            Console.WriteLine("\tUsage:");
+            Console.WriteLine("\t\tNdst MODE input1 (input2) output\n");
+            Console.WriteLine("\tModes:");
+            Console.WriteLine("\t\t-e Extract a ROM (input1) to a folder (output).");
+            Console.WriteLine("\t\t-p Pack a ROM folder (input1) and patch folder (input2) to a ROM (output).");
         }
 
     }
